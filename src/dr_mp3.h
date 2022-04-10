09a3cec4eb4a256d717a107c5390cfad5542ce26
Ozkan Sezer <sezeroz@gmail.com>
2022-04-10 04:33:50 +0300

    fix SSE2 builds after previous ARM NEON commit.

--- src/dr_mp3.h
+++ src/dr_mp3.h
@@ -2117,7 +2117,11 @@ static void drmp3d_synth(float *xl, drmp3d_sample_t *dstl, int nch, float *lins)
             vst1_lane_s16(dstl + (49 + i)*nch, pcmb, 2);
 #endif
 #else
+#if DRMP3_HAVE_SSE
+            static const drmp3_f4 g_scale = { 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f, 1.0f/32768.0f };
+#else /* NEON: */
             const drmp3_f4 g_scale = vdupq_n_f32(1.0f/32768.0f);
+#endif
             a = DRMP3_VMUL(a, g_scale);
             b = DRMP3_VMUL(b, g_scale);
 #if DRMP3_HAVE_SSE
