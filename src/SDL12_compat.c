183bac11d2060c68ace55c4396c951c97db4db81
Ryan C. Gordon <icculus@icculus.org>
2022-09-01 11:38:21 -0400

    init: Better assertion check for init flags during SDL_Quit().

--- src/SDL12_compat.c
+++ src/SDL12_compat.c
@@ -2488,13 +2488,14 @@ SDL_QuitSubSystem(Uint32 sdl12flags)
     }
 
     InitializedSubsystems20 &= ~sdl20flags;
+    InitializedSubsystems20 &= ~SDL_INIT_NOPARACHUTE;  /* SDL2 accepts this flag but ignores it. */
 }
 
 DECLSPEC void SDLCALL
 SDL_Quit(void)
 {
     SDL_QuitSubSystem(SDL_WasInit(0) | SDL12_INIT_CDROM);
-    SDL_assert(InitializedSubsystems20 == 0);
+    SDL_assert((InitializedSubsystems20 == 0) || (SDL12Compat_GetHintBoolean("SDL12COMPAT_NO_QUIT_VIDEO", SDL_FALSE) && (InitializedSubsystems20 == SDL_INIT_VIDEO)));
 }
 
 DECLSPEC void SDLCALL
