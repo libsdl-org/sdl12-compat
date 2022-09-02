6402c283983ada5e6f38dfc7e95085b3d343a841
Ozkan Sezer <sezeroz@gmail.com>
2022-09-02 23:47:56 +0300

    SDL_version.h: really update version.

--- include/SDL/SDL_version.h
+++ include/SDL/SDL_version.h
@@ -45,7 +45,7 @@ typedef struct SDL_version
 #define SDL_VERSION(X) { \
     (X)->major = 1; \
     (X)->minor = 2; \
-    (X)->patch = 50; \
+    (X)->patch = 54; \
 }
 
 #define SDL_VERSIONNUM(X, Y, Z) ((X)*1000 + (Y)*100 + (Z))
