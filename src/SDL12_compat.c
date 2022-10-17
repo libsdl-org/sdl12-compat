libsdl-org/sdl12-compat

commit 2a96bb35d7562f75c710e5824a49eb14c13c1bac

quirks: Force X11 on Linux if certain symbols are in the process.
So if we see the process references GLEW or Nvidia Cg, we assume the
app will fail if it can't talk to glX.

This automates the process in many cases instead of having to list these
specific titles in the quirks table.

This patch in its initial form, and the idea, are from @smvc (thanks!).

Fixes #249.
Fixes #252.
Fixes #258.
Fixes #262.
Fixes #263.

src/SDL12_compat.c
@@ -1189,11 +1189,9 @@ static QuirkEntryType quirks[] = {
 #if defined(__unix__)
     /* Awesomenauts uses Cg, and does weird things with the GL context. */
     {"Awesomenauts.bin.x86", "SDL12COMPAT_OPENGL_SCALING", "0"},
-    {"Awesomenauts.bin.x86", "SDL_VIDEODRIVER", "x11"},
     {"Awesomenauts.bin.x86", "SDL12COMPAT_FORCE_GL_SWAPBUFFER_CONTEXT", "1"},
 
     /* Braid uses Cg, which uses glXGetProcAddress(). */
-    {"braid", "SDL_VIDEODRIVER", "x11"},
     {"braid", "SDL12COMPAT_OPENGL_SCALING", "0"},
 
     /* GOG's DOSBox builds have architecture-specific filenames. */
@@ -1329,6 +1327,30 @@ SDL12Compat_ApplyQuirks(void)
             }
         }
     }
+
+    #ifdef __linux__
+    {
+        void *global_symbols = dlopen(NULL, RTLD_LOCAL|RTLD_NOW);
+        SDL_bool force_x11 = SDL_FALSE;
+
+        /* Use linked libraries to detect what quirks we are likely to need */
+        if (global_symbols != NULL) {
+            /* GLEW (e.g. Frogatto, SLUDGE) */
+            if (dlsym(global_symbols, "glxewInit") != NULL) { force_x11 = SDL_TRUE; }
+            /* NVIDIA Cg (e.g. Awesomenauts, Braid) */
+            else if (dlsym(global_symbols, "cgGLEnableProgramProfiles") != NULL) { force_x11 = SDL_TRUE; }
+        }
+
+        dlclose(global_symbols);
+
+        if (force_x11) {
+            SDL20_Log("sdl12-compat: We are forcing this app to use X11, because it probably");
+            SDL20_Log("sdl12-compat: talks to an X server directly, outside of SDL. If possible,");
+            SDL20_Log("sdl12-compat: this app should be fixed, to be compatible with Wayland, etc.");
+            SDL20_setenv("SDL_VIDEODRIVER", "x11", 1);
+        }
+    }
+    #endif
 }
 
 static int

commit 0fb5c56eaa5c74044c0830909627935718dd02f2

quirks: Fixes for symbol-based quirks to force X11
Make some changes to how the automatic quirks for apps which link
against gl[x]ew and Cg functions work. In particular:
- They can be overridden by a user-set SDL_VIDEODRIVER env variable.
- They only print warnings if debug logging
  (SDL12COMPAT_DEBUG_LOGGING=1) is enabled, to match the other quirks.
- The messages are no-longer wrapped.

Fixes: 2a96bb35d756 ("quirks: Force X11 on Linux if certain symbols are in the process.")

src/SDL12_compat.c
@@ -1307,6 +1307,7 @@ static void
 SDL12Compat_ApplyQuirks(void)
 {
     const char *exe_name = SDL12Compat_GetExeName();
+    const char *videodriver_env = SDL20_getenv("SDL_VIDEODRIVER");
     int i;
 
     if (*exe_name == '\0') {
@@ -1329,6 +1330,7 @@ SDL12Compat_ApplyQuirks(void)
     }
 
     #ifdef __linux__
+    if (!videodriver_env || SDL20_strcmp(videodriver_env, "x11"))
     {
         void *global_symbols = dlopen(NULL, RTLD_LOCAL|RTLD_NOW);
         SDL_bool force_x11 = SDL_FALSE;
@@ -1344,10 +1346,14 @@ SDL12Compat_ApplyQuirks(void)
         dlclose(global_symbols);
 
         if (force_x11) {
-            SDL20_Log("sdl12-compat: We are forcing this app to use X11, because it probably");
-            SDL20_Log("sdl12-compat: talks to an X server directly, outside of SDL. If possible,");
-            SDL20_Log("sdl12-compat: this app should be fixed, to be compatible with Wayland, etc.");
-            SDL20_setenv("SDL_VIDEODRIVER", "x11", 1);
+            if (!videodriver_env) {
+                if (WantDebugLogging) {
+                    SDL20_Log("Forcing this app to use X11, because it probably talks to an X server directly, outside of SDL. If possible, this app should be fixed, to be compatible with Wayland, etc.");
+                }
+                SDL20_setenv("SDL_VIDEODRIVER", "x11", 1);
+            } else if (videodriver_env && WantDebugLogging) {
+                SDL20_Log("This app looks like it requires X11, but the SDL_VIDEODRIVER environment variable is set to \"%s\". If you have issues, try setting SDL_VIDEODRIVER=x11", videodriver_env);
+            }
         }
     }
     #endif