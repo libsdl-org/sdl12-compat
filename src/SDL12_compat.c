libsdl-org/sdl12-compat

commit 7be4b3c99a436e360a43aefc929087c46cdadee9

silenced warnings from SDL12COMPAT_MAX_VIDMODE parsing.

src/SDL12_compat.c
@@ -2079,10 +2079,10 @@ Init12VidModes(void)
 
     maxmodestr = SDL12Compat_GetHint("SDL12COMPAT_MAX_VIDMODE");
     if (maxmodestr) {
-        unsigned int w, h;
+        unsigned int w = 0, h = 0;
         SDL_sscanf(maxmodestr, "%ux%u", &w, &h);
-        maxw = (Uint16) SDL_clamp(w, 0, 0xFFFF);
-        maxh = (Uint16) SDL_clamp(h, 0, 0xFFFF);
+        if (w > 0xFFFF) w = 0xFFFF;
+        if (h > 0xFFFF) h = 0xFFFF;
     }
 
     for (i = 0; i < total; ++i) {

commit 3b79fe4b8609a2a612beee664a2b4f4ac5c011ff

fix SDL12COMPAT_MAX_VIDMODE,
.. was broken by commit 7be4b3c99a436e360a43aefc929087c46cdadee9

src/SDL12_compat.c
@@ -2119,6 +2119,8 @@ Init12VidModes(void)
         SDL_sscanf(maxmodestr, "%ux%u", &w, &h);
         if (w > 0xFFFF) w = 0xFFFF;
         if (h > 0xFFFF) h = 0xFFFF;
+        maxw = w;
+        maxh = h;
     }
 
     for (i = 0; i < total; ++i) {